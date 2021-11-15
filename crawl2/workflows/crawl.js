e805dc69329e4bdabcc0d1b260c8aa4f34af3a39
yoko98 <62095461+yoko98@users.noreply.github.com>
2021-11-16 06:22:51 +0800

    Updated crawl.js

--- crawl2/workflows/crawl.js
+++ crawl2/workflows/crawl.js
@@ -1,44 +1,36 @@
-/*****1. Accept user input && Get the url******/
-//const input = await Apify.getInput();
-//console.log(input);
-
 const Apify = require('apify');
-Apify.main(async()=>{
-    
-    //2a. create a requestList - list of Urls
-    const requestList = await Apify.openRequestList('start-urls',[
-            {url: 'https://www.hati.my/organisations/'},
-    ]);
-    const requestQueue = await Apify.openRequestQueue('some-url');
+
+Apify.main(async () => {
     const dataJson = [];
+    const titleList = [];
+    // create a dataset and open it
+    const dataset = await Apify.openDataset('crawl-page');
+    // Create a RequestQueue
+    const requestQueue = await Apify.openRequestQueue();
+    // Define the starting URL
+    await requestQueue.addRequest({ url: 'https://www.hati.my/organisations/' });
+    
+    // Create a CheerioCrawler
     const crawler = new Apify.CheerioCrawler({
-        requestList,
-        //function called for each url
-        handlePageFunction: async ({request,response, $}) => {
+        requestQueue,
+        handlePageFunction: async ({ request,response, $ }) => {
             $('.row div').each((index, el) =>{
-                const orgList = [];
-                //reduce the set of matched elements to the one at the specified index
+                //find the link and title of each category in the page crawled
                 href = $(el).find('.icon-box').attr('href');
                 title = $(el).find('.entry-title').text();
-                
-                /*handlePageFunction: async ({request,response, content, $}) => {
-                    $('.left-area div').each((index, el) =>{
-                        org_href = $(el).find('.entry-title2').attr('href');
-                        org_title = $(el).find('.entry-title2').text();
-                        org_content = $(el).find('.content').text(); 
-                        orgList.push([org_href, org_title, org_content]);
-                    });
-                    
-                });*/
-                dataJson.push([href, title]);
+                titleList.push(title);
+                dataJson.push([
+                        `Url: ${href}`, 
+                        `Category: ${title}`, ]);
+                //if title and url is undefined or null, do not include in the json file
+                if (title == ''){
+                    dataJson.pop();
+                }
             });
-            //store data to default dataset
-            await Apify.pushData({dataJson});
+            dataJson.push(`URL Category: ${request.url}`);
         },
     });
-    //Run the crawler
+    // Run the crawler
     await crawler.run();
-    console.log('Crawler and Scrape finished.');
+    await dataset.pushData({dataJson});
 });
-
-    
